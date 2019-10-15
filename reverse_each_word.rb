5a977b4c736d166a4a9ee75bdda073c59e68a136
Kevin Kim <kevin.kim1030@gmail.com>
2019-10-15 01:53:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,8 @@
-DEF REVERSE_EACH_
+def reverse_each_word(string)
+  new_array = string.split(" ")
+  array = []
+  new_array.each do |string|
+    array << string.reverse
+  end
+  array.join(" ")
+end
